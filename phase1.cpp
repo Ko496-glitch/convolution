0946b76b1f7671e272bee21726d9a60c79853e22
kartikohlan <kartik7ohlan@gmail.com>
2026-02-17 11:33:23 -0500

    wrote mul_norm for brute force

--- phase1.cpp
+++ phase1.cpp
@@ -4,26 +4,49 @@
 #include<NTL/vec_ZZ_p.h>
 #include<NTL/vector.h>
 
-constexpr const PRIME_BIT  = 60;
-constexpr const Err_bound =  80;
+constexpr const long PRIME_BIT  = 60;
+constexpr const long Err_bound =  80;
 using namespace NTL;
 
 /* O(n^2) compute for toeplitz matrix with input vector Benchmarking against Cooley method */
+/* from 1d -> 2d we use V[(n-1) + ( j- i) * xj)] j: 0 -> n */
 
-/*
-void mul_norm(vec_ZZ_p &input, vec_ZZ_p &toep_mat, int n){
+vec_ZZ_p mul_norm(const vec_ZZ_p &input, const  vec_ZZ_p &tope_mat, long n){
+    vec_ZZ_p res;
+    res.SetLength(n);
+
+    for(long i{};i<n;++i){
+        ZZ_p sum{};
+        for(long j{};j < n;++j){
+            long v  = (n - 1) + (j - i);
+            ZZ_p prod;
+            mul(prod, tope_mat[v], input[j]);
+            add(sum, sum, prod);
+        }
+        tope_mat = input[i];
+        res[i] = tope_mat[i];
+    }
+    return res;
+}
+
+/* optimized mutliplation of tope_mat * input vector. O(n log n). Would be benchmark against mul_norm */
+
+vec_zz_p cooley_mul(const vec_ZZ_p &input, const vec_ZZ_p &tope_mat,int n ){
+    vec_zz_p res;
 
     for(long i{};i<n;++i){
 
     }
 
+
+
+    return res;
 }
-*/
 
 int main(){
 
   ZZ p;
-  GenPrime(p ,k ,err);
+  GenPrime(p ,PRIME_BIT ,Err_bound);
   ZZ_p::init(p);
   long n = RandomBnd(1024) + 1;
   SetSeed(to_ZZ(time(nullptr)));
